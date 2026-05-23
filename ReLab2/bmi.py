513e3206d70d0a041d0c540d8d9537d44ec860dd
Sarah <SARAHT.25@ichat.sp.edu.sg>
2026-05-23 20:05:19 +0800

    Add ReLab2 files

---
+++ ReLab2/bmi.py
@@ -0,0 +1,21 @@
+def calculate_bmi(height, weight):
+    print("Height=" + str(height)) #str() convert height from int to str
+    print("Weight=" + str(weight)) #str() convert height from float to str
+
+    #Add code here to calculate bmi
+    bmi = weight/height**2 # weight divided by height to the power of 2
+
+    #Add code here to display calculated bmi
+    print(f"bmi={bmi}")
+
+    if bmi <18.5:
+        print("Underweight")
+    elif bmi<= 25.0:
+        print("Normal Weight")
+    else:
+        print("Overweight")
+
+calculate_bmi(weight=57,height=1.73) #here matches by name
+
+
+
